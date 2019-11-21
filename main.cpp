f9972294d593df9afcb02f200f536bf737345295
João Paulo <joaovaz94.4@gmail.com>
2019-11-21 09:56:07 -0300

    Limpeza das impressões de tela e definições de objetos no arquivo main
    limpeza de impressões na função main para incluir a interface de texto no programa.

--- main.cpp
+++ main.cpp
@@ -35,103 +35,24 @@ int main()
     TUTipo testeTipo;
 
     cout << endl << endl;
+
     //!Execução dos Testes de Unidade dos Domínios
-    switch(testeData.executar()){
-        case TUData::SUCESSO: cout << "==  TESTE DE UNIDADE DE DATA: SUCESSO ==" << endl << endl;
-                                break;
-        case TUData::FALHA  : cout << "==  TESTE DE UNIDADE DE DATA: FALHA ==" << endl << endl;
-                                break;
-    }
-    switch(testeCpf.executar()){
-        case TUCpf::SUCESSO: cout << "==  TESTE DE UNIDADE DE CPF: SUCESSO ==" << endl << endl;
-                                break;
-        case TUCpf::FALHA  : cout << "==  TESTE DE UNIDADE DE CPF: FALHA ==" << endl << endl;
-                                break;
-    }
-    switch(testeHorario.executar()){
-        case TUHorario::SUCESSO: cout << "==  TESTE DE UNIDADE DE HORÁRIO: SUCESSO ==" << endl << endl;
-                                break;
-        case TUHorario::FALHA  : cout << "==  TESTE DE UNIDADE DE HORÁRIO: FALHA ==" << endl << endl;
-                                break;
-    }
-    switch(testeCodigo.executar()){
-        case TUCodigo::SUCESSO: cout << "==  TESTE DE UNIDADE DE CÓDIGO: SUCESSO ==" << endl << endl;
-                                break;
-        case TUCodigo::FALHA  : cout << "==  TESTE DE UNIDADE DE CÓDIGO: FALHA ==" << endl << endl;
-                                break;
-    }
-    switch(testePreco.executar()){
-        case TUPreco::SUCESSO: cout << "==  TESTE DE UNIDADE DE PREÇO: SUCESSO ==" << endl << endl;
-                                break;
-        case TUPreco::FALHA  : cout << "==  TESTE DE UNIDADE DE PREÇO: FALHA ==" << endl << endl;
-                                break;
-    }
-    switch(testeDisp.executar()){
-        case TUDisp::SUCESSO: cout << "==  TESTE DE UNIDADE DE DISPONIBILIDADE: SUCESSO ==" << endl << endl;
-                                break;
-        case TUDisp::FALHA  : cout << "==  TESTE DE UNIDADE DE DISPONIBILIDADE: FALHA ==" << endl << endl;
-                                break;
-    }
-    switch(testeSenha.executar()){
-        case TUSenha::SUCESSO: cout << "==  TESTE DE UNIDADE DE SENHA: SUCESSO ==" << endl << endl;
-                                break;
-        case TUSenha::FALHA  : cout << "==  TESTE DE UNIDADE DE SENHA: FALHA ==" << endl << endl;
-                                break;
-    }
-    switch(testeCodVer.executar()){
-        case TUCodVer::SUCESSO: cout << "==  TESTE DE UNIDADE DE CÓDIGO DE VERIFICAÇÃO: SUCESSO ==" << endl << endl;
-                                break;
-        case TUCodVer::FALHA  : cout << "==  TESTE DE UNIDADE DE CÓDIGO DE VERIFICAÇÃO: FALHA ==" << endl << endl;
-                                break;
-    }
-    switch(testeCodJogo.executar()){
-        case TUCodJogo::SUCESSO: cout << "==  TESTE DE UNIDADE DE CÓDIGO DE JOGO: SUCESSO ==" << endl << endl;
-                                break;
-        case TUCodJogo::FALHA  : cout << "==  TESTE DE UNIDADE DE CÓDIGO DE JOGO: FALHA ==" << endl << endl;
-                                break;
-    }
-    switch(testeCodIng.executar()){
-        case TUCodIng::SUCESSO: cout << "==  TESTE DE UNIDADE DE CÓDIGO DE INGRESSO: SUCESSO ==" << endl << endl;
-                                break;
-        case TUCodIng::FALHA  : cout << "==  TESTE DE UNIDADE DE CÓDIGO DE INGRESSO: FALHA ==" << endl << endl;
-                                break;
-    }
-    switch(testeEstado.executar()){
-        case TUEstado::SUCESSO: cout << "==  TESTE DE UNIDADE DE ESTADO: SUCESSO ==" << endl << endl;
-                                break;
-        case TUEstado::FALHA  : cout << "==  TESTE DE UNIDADE DE ESTADO: FALHA ==" << endl << endl;
-                                break;
-    }
-    switch(testeNumCartao.executar()){
-        case TUNumCartao::SUCESSO: cout << "==  TESTE DE UNIDADE DE NÚMERO DO CARTÃO DE CRÉDITO: SUCESSO ==" << endl << endl;
-                                break;
-        case TUNumCartao::FALHA  : cout << "==  TESTE DE UNIDADE DE NÚMERO DO CARTÃO DE CRÉDITO: FALHA ==" << endl << endl;
-                                break;
-    }
-    switch(testeValidade.executar()){
-        case TUValidade::SUCESSO: cout << "==  TESTE DE UNIDADE DA DATA DE VALIDADE: SUCESSO ==" << endl << endl;
-                                break;
-        case TUValidade::FALHA  : cout << "==  TESTE DE UNIDADE DA DATA DE VALIDADE: FALHA ==" << endl << endl;
-                                break;
-    }
-    switch(testeNomeJogo.executar()){
-        case TUNomeJogo::SUCESSO: cout << "==  TESTE DE UNIDADE DO NOME DO JOGO: SUCESSO ==" << endl << endl;
-                                break;
-        case TUNomeJogo::FALHA  : cout << "==  TESTE DE UNIDADE DO NOME DO JOGO: FALHA ==" << endl << endl;
-                                break;
-    }
-    switch(testeCidade.executar()){
-        case TUCidade::SUCESSO: cout << "==  TESTE DE UNIDADE DA CIDADE: SUCESSO ==" << endl << endl;
-                                break;
-        case TUCidade::FALHA  : cout << "==  TESTE DE UNIDADE DA CIDADE: FALHA ==" << endl << endl;
-                                break;
-    }
-    switch(testeTipo.executar()){
-        case TUTipo::SUCESSO: cout << "==  TESTE DE UNIDADE DO TIPO: SUCESSO ==" << endl << endl;
-                                break;
-        case TUTipo::FALHA  : cout << "==  TESTE DE UNIDADE DO TIPO: FALHA ==" << endl << endl;
-                                break;
-    }
+    testeData.executar();
+    testeCpf.executar();
+    testeHorario.executar();
+    testeCodigo.executar();
+    testePreco.executar();
+    testeDisp.executar();
+    testeSenha.executar();
+    testeCodVer.executar();
+    testeCodJogo.executar();
+    testeCodIng.executar();
+    testeEstado.executar();
+    testeNumCartao.executar();
+    testeValidade.executar();
+    testeNomeJogo.executar();
+    testeCidade.executar();
+    testeTipo.executar();
 
     //!Declaração de objetos dos Dompinios
     Data d1;
@@ -158,6 +79,7 @@ int main()
     Ingresso ing;
     CartaoCredito cart;
 
+    /*
     string cpf = "23261322110";
     string cod = "01030";
     string dt = "23/10/1917";
@@ -174,7 +96,9 @@ int main()
     int tip = 1;
     double prc=30.05;
     int dsp=30;
+    */
 
+    /*
     //!definições dos domínios
     c1.defineCpf(cpf);
     cd1.defineCodigo(cod);
@@ -192,7 +116,9 @@ int main()
     nj1.defineNome(njo);
     cdd1.defineNome(cid);
     t1.defineTipo(tip);
+    */
 
+    /*
     //!definições de uma partida
     part.defineCodigo(cd1);
     part.defineData(d1);
@@ -219,6 +145,9 @@ int main()
     cart.defineCodVer(cv1);
     cart.defineValidade(v1);
 
+    */
+
+    /*
     //!Impressão de domínios
     cout << "CPF: " << c1.pegaCpf() << endl;
     cout << "Data: " << d1.viraString() << endl;
@@ -236,9 +165,11 @@ int main()
     cout << "Nome do Jogo: " << nj1.pegaNome() << endl;
     cout << "Cidade: " << cdd1.pegaNome() << endl;
     cout << "Tipo: " << t1.stringTipo() << endl;
+    */
 
-    cout << endl << endl;
+    //cout << endl << endl;
 
+    /*
     //!Impressão de Partida
     part.imprimePartida();
 
@@ -253,7 +184,7 @@ int main()
 
     //!impressão de Cartão de Crédito
     cart.imprimeIngresso();
-
+    */
 
     return 0;
 }
