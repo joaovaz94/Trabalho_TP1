f100e8b7caaa293c6014dfbc10bb916fc3ed4b61
João Paulo <joaovaz94.4@gmail.com>
2019-11-22 21:20:40 -0300

    Começo de Implementação da classe Interface

--- Interface.h
+++ Interface.h
@@ -1,5 +1,21 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
+class Interface
+{
+    private:
+        char operacao;
+
+    public:
+        Interface() {}
+        static Interface& pegaInstancia() {//Singleton
+            static Interface instancia;
+            return instancia;
+        }
+
+        Interface(Interface const&) = delete;//Singleton
+        void operador() = delete;//Singleton
+        //!
+};
 
 #endif // INTERFACE_H
